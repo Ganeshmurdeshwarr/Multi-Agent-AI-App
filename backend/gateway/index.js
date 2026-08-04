06fcb94e32fb1cc6a7d59c15ee1f34f87897dfaf
Ganesh Devadiga <ganeshmurdeshwar@gmail.com>
2026-08-04 19:27:18 +0530

    cicd test

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -30,7 +30,7 @@ console.log("BILLING_SERVICE:", process.env.BILLING_SERVICE);
 app.use("/api/me", protect, getCurrentUser);
 
 app.use("/", (req, res) => {
-  res.json({ message: `Gateway received request for ` });
+  res.json({ message: `Gateway received request for ALL IS WELL` });
 });
 
 app.listen(port, () => {
