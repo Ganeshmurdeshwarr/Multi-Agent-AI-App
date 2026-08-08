299839c57e54833076776d91bf370f88539f3ad5
Ganesh Devadiga <ganeshmurdeshwar@gmail.com>
2026-08-08 17:41:28 +0530

    increase gateway proxy timeout

--- backend/gateway/utils/proxyWithHeader.js
+++ backend/gateway/utils/proxyWithHeader.js
@@ -1,7 +1,9 @@
 import proxy from "express-http-proxy"
 
 export const proxyWithHeader=(serviceUrl)=>{
+    
     return proxy(serviceUrl,{
+        timeout: 60000,
         proxyReqOptDecorator:(proxyReqOpts , srcReq)=>{
             if(srcReq.user){
                 proxyReqOpts.headers['X-user-id']=srcReq.user._id
